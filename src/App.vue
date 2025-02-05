20e133d8ba0ae0d7b98bbf115cb552055e331847
Mateusz Jablonowski <mateusz.jablonowski@dbschenker.com>
2025-02-05 01:45:11 +0100

    styles + more e2e tests

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
     <form v-on:submit.prevent="addNewTodo">
-      <label for="new-todo">Add a todo</label>
-      <input
-        type="text"
-        v-model="newTodoText"
-        id="new-todo"
-        placeholder="E.g. do the dishes"
-      />
-      <button id="add-todo">Add</button>
+      <input class="add-input" type="text" v-model="newTodoText" id="new-todo" placeholder="Add a todo e.g. call mom" />
+      <button class="add-btn" id="add-todo">Add</button>
     </form>
-    <ul id="todo-list">
-      <todo-item
-        v-for="(todo, index) in todos"
-        :key="todo.id"
-        :title="todo.title"
-        @remove="todos.splice(index, 1)"
-      ></todo-item>
+    <ul class="todo-list" id="todo-list">
+      <todo-item v-for="(todo, index) in todos" :key="todo.id" :title="todo.title"
+        @remove="todos.splice(index, 1)"></todo-item>
     </ul>
   </div>
 </template>
@@ -33,7 +23,7 @@ export default {
       newTodoText: "",
       todos: [
       ],
-      nextTodoId: 4,
+      nextTodoId: 0,
     };
   },
   methods: {
@@ -49,4 +39,56 @@ export default {
 </script>
 
 <style>
+.todo-list {
+  list-style-type: none;
+  padding: 0;
+}
+
+.add-btn {
+  cursor: pointer;
+  outline: 0;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 1rem;
+  border-radius: .25rem;
+  transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+  color: #0d6efd;
+  border-color: #0d6efd;
+
+  :hover {
+    color: #fff;
+    background-color: #0d6efd;
+    border-color: #0d6efd;
+  }
+
+}
+
+.add-input {
+  margin-right: 6px;
+  padding: 6px 12px;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  appearance: none;
+  border-radius: 4px;
+  transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+
+  :focus {
+    color: #212529;
+    background-color: #fff;
+    border-color: #86b7fe;
+    outline: 0;
+    box-shadow: 0 0 0 0.25rem rgb(13 110 253 / 25%);
+  }
+
+}
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <li>
+  <li class="todo-item">
     {{ title }}
-    <button @click="$emit('remove')">Remove</button>
+    <button class="remove-btn" @click="$emit('remove')">X</button>
   </li>
 </template>
 
@@ -12,3 +12,32 @@ export default {
   emits: ["remove"],
 };
 </script>
+
+<style scoped>
+.todo-item {
+  margin: 0 0 0 11px;
+  font-size: 16px;
+  font-weight: 400;
+  color: #212529;
+  background-color: #fff;
+  font-family: Arial, sans-serif;
+}
+
+.remove-btn {
+  cursor: pointer;
+  outline: 0;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  background-color: transparent;
+  /* border: 1px solid transparent; */
+  padding: 6px 12px;
+  font-size: 1rem;
+  /* border-radius: .25rem; */
+  /* transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out; */
+  color: #dc3545;
+  /* border-color: #dc3545; */
+  border: none;
+}
+</style>
